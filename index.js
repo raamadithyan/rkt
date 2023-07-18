a71cd2df55098d65fb2f7495f1ec581310821bcb
raamadithyan <chitha.don@gmail.com>
2023-07-18 21:34:36 +0530

    added action and reducer

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('server bruh')
+const CAKE_ORDERD= 'CAKE_ORDERED';
+
+
+function orderCake(){
+    return {
+        type:CAKE_ORDERD,
+        quantity:1, 
+    }
+}
+
+const initialState = {
+    numofCakes : 10,
+    
+}
+
+function reducer(state = initialState,action){
+    switch(action.type){
+        case CAKE_ORDERD:
+            return{
+                numofCakes:state.numofCakes-1,
+            }
+            default:
+                return state;
+    }
+
+}
